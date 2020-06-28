dfc8bebe3beadd78052f881299b2143e548a5d66
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-06-28 11:44:33 +0200

    first naive implementation

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/sebnyberg/protoio
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.2
-	github.com/kr/pretty v0.2.0 // indirect
-	go.uber.org/zap v1.15.0 // indirect
+	github.com/golang/protobuf v1.4.1
 	google.golang.org/protobuf v1.25.0
 )
--- protoio.go
+++ protoio.go
@@ -1,1 +1,53 @@
 package protoio
+
+import (
+	"encoding/binary"
+	"fmt"
+	"google.golang.org/protobuf/proto"
+	"io"
+)
+
+type Writer interface {
+	WriteMsg(proto.Message) error
+	io.Closer
+}
+
+type writer struct {
+	w io.Writer
+}
+
+func NewWriter(w io.Writer) Writer {
+	return &writer{
+		w: w,
+	}
+}
+
+// WriteMsg writes one protobuf message to the embedded writer
+func (w *writer) WriteMsg(m proto.Message) error {
+	// Write length of message
+	lenBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(lenBuf, uint64(proto.Size(m)))
+	_, err := w.w.Write(lenBuf)
+	if err != nil {
+		return fmt.Errorf("failed to write msg length, err: %w", err)
+	}
+
+	// Write message
+	data, err := proto.Marshal(m)
+	if err != nil {
+		return fmt.Errorf("failed to marshal msg, err: %w", err)
+	}
+	_, err = w.w.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to write msg, err: %w", err)
+	}
+
+	return nil
+}
+
+func (w *writer) Close() error {
+	if closer, ok := w.w.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
+}
